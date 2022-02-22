0ef9e1643c5b7f471f18565033f54af87093f1fe
Michi <mic.michael@gmx.de>
2022-02-22 23:33:48 +0100

    sed fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,14 +3,14 @@ if [ -n "$SERVICE_PORT" ]; then
     sed -i "s/PRT = 8000/PRT = ${SERVICE_PORT}/" Http.py
 fi
 if [ -n "$MODEL_LOCATION" ]; then
-    sed -i "s|MODEL_FILE = b\"./GoogleNews-vectors-negative300.bin\"|MODEL_FILE = b\"${MODEL_LOCATION}\"" Word2Vec.py
+    sed -i "s|MODEL_FILE = b\"./GoogleNews-vectors-negative300.bin\"|MODEL_FILE = b\"${MODEL_LOCATION}\"|" Word2Vec.py
 else
     MODEL_LOCATION="./model_file.bin"
 fi
 if [ -n "$MODEL_URL" ]; then
     echo "Downloading pre-trained model..."
     wget ${MODEL_URL} -o ${MODEL_LOCATION}
-    sed -i "s|MODEL_FILE = b\"./GoogleNews-vectors-negative300.bin\"|MODEL_FILE = b\"${MODEL_LOCATION}\"" Word2Vec.py
+    sed -i "s|MODEL_FILE = b\"./GoogleNews-vectors-negative300.bin\"|MODEL_FILE = b\"${MODEL_LOCATION}\"|" Word2Vec.py
 fi
 
 python3 Http.py
